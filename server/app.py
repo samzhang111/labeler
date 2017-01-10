7fc1a080e20f0232e6d8080b8c1588c0250e6875
Sam Zhang <shimian.zhang@gmail.com>
2017-01-10 06:42:50 +0100

    HTTP Basic Auth

--- server/app.py
+++ server/app.py
@@ -3,6 +3,40 @@ from server.app_project import project
 
 app = Flask(__name__)
 
+from functools import wraps
+from flask import request, Response
+import os
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    actual_username = os.getenv('FLASK_USER', None)
+    actual_password = os.getenv('FLASK_PASSWORD', None)
+
+
+    if not actual_username or not actual_password:
+        return True
+
+    return username == actual_username and password == actual_password
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route('/labels')
 def get_labels():
     return jsonify({
@@ -27,6 +61,7 @@ def post_label(post_id):
     return jsonify({'index': post_id})
 
 @app.route('/')
+@requires_auth
 def index():
     return render_template('index.html')
 
